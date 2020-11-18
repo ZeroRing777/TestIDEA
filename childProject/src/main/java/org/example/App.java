cda43edbd0c99d17de1b606ce54ff52f35dda881
KONGLING ZHANG <kz172@scarletmail.rutgers.edu>
2020-11-18 15:32:35 +0800

    Initial commit

---
+++ childProject/src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String dateStr = "2012-12-12 12:12:12";
+
+    }
+}
